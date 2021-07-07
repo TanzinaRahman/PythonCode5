7e33665eb7364d0c4b6f4f78088d324adb0e28f4
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 00:12:14 +0600

    Create PythonProgram5.py

---
+++ PythonProgram5.py
@@ -0,0 +1,18 @@
+'''
+
+Getting User Input
+
+'''
+
+name= input("Enter your name :")
+age=  input("Enter your age  :")
+cgpa=input("Enter your cgpa :")
+
+
+print("............")
+
+
+
+print("name : " + name)
+print("age  :  " + age)
+print("cgpa : " + cgpa)
